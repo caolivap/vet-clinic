de3a1b185f4c597e30c5d02f0932f76ced18c7bb
Carlos Oliva <caoliva@sura.com.co>
2019-01-15 10:49:16 -0500

    Utilizando Factory Class

--- src/main/java/serenitylabs/tutorials/vetclinic/playingball/model/Child.java
+++ src/main/java/serenitylabs/tutorials/vetclinic/playingball/model/Child.java
@@ -11,24 +11,7 @@ public class Child {
     private PlayTennis playTennis = new PlayTennis();
 
     public void goPlay(Game game) {
-        Player gameToPlay = gameCalled(game);
+        Player gameToPlay = PlayerForGame.called(game);
         gameToPlay.play();
-    }
-
-    private Player gameCalled(Game game) {
-        switch (game){
-            case Football:
-                return new PlayFootball();
-            case Tennis:
-                return new PlayTennis();
-            case Cricket:
-                return new PlayCricket();
-            case Handball:
-                return new PlayHandball();
-            case Hockey:
-                return new PlayHockey();
-             default:
-                 return null;
-        }
     }
 }
