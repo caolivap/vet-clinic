f0dc4a652fb302c7125519cefd5551f1beeee6ac
Carlos Oliva <caoliva@sura.com.co>
2019-01-21 09:28:08 -0500

    Solución Lambdas Java

--- src/main/java/serenitylabs/tutorials/vetclinic/sales/TaxRateCalculator.java
+++ src/main/java/serenitylabs/tutorials/vetclinic/sales/TaxRateCalculator.java
@@ -1,10 +1,9 @@
 package serenitylabs.tutorials.vetclinic.sales;
 
+import java.util.function.Function;
 import serenitylabs.tutorials.vetclinic.sales.model.LineItem;
 
 /**
  * Created by john on 03/07/17.
  */
-public interface TaxRateCalculator {
-    TaxRate rateFor(LineItem item);
-}
+public interface TaxRateCalculator extends Function<LineItem, TaxRate> {}
